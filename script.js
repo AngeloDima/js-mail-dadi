93f074fdfdf1ce3c4a2f02aaf04b5134c3e63329
Angelo <Angelodimauro91@gmail.com>
2022-09-07 09:54:44 +0200

    creazione vari giocatori

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+// Giocatore umano
+const playerScore =  Math.floor( Math.random() * 6 ) + 1
+console.log('playerScore', playerScore);
+
+// Giocatore PC
+const pcScore =  Math.floor( Math.random() * 6 ) + 1
+console.log('pcScore', pcScore);
