24955a6fd39910a9b8eeb2cef20df62b42c9d302
Angelo <Angelodimauro91@gmail.com>
2022-09-07 09:55:49 +0200

    Condizioni e finish

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ console.log('playerScore', playerScore);
 // Giocatore PC
 const pcScore =  Math.floor( Math.random() * 6 ) + 1
 console.log('pcScore', pcScore);
+
+
+// se punteggio giocatore > PC vince giocatore
+
+if (playerScore > pcScore){
+    console.log('vince il giocatore');
+} else if (pcScore > playerScore){
+     // se punteggio PC > giocatore vince PC
+    console.log('vince il PC');
+   
+} else {
+    // altrimenti uguale parità
+    console.log('pareggio');
+}
